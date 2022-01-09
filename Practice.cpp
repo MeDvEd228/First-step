c6da45f75505d1d0984f13a11c2900f9ebec637f
MeDvEd228 <88885539+MeDvEd228@users.noreply.github.com>
2022-01-10 00:48:17 +0500

    simple circle animation
    I made a simple cirlce animation. Following the example from the channel Onigiri.
    
     Сделал простую анимацию окружности из символов. почти под чистую списал код у одного ютуб канала "Onigiri". Сам код не очень, попытки как то его менять в моем случае приводили к неудачам, была задумка сделать анимацию как будто монетка крутится, возможно позже сделаю. Если убрать пару строчек получится круг.

---
+++ Practice.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+
+void Delenie() //деление числа на разряды 
+{
+	int NUM = 10819;
+	int arr[5];
+	int i = 0;
+	while (NUM != 0)
+	{
+		arr[i] = NUM % 10;
+		NUM /= 10;
+		i++;
+	}
+	for (int j = 5; j != 0; j--)
+	{
+		std::cout << arr[j - 1] << std::endl;
+	}
+}
+
+
+int main()
+{
+	int const  WIDTH_SCREEN = 120;
+	int const  HEIGHT_SCREEN = 30;
+	float aspect = (float)WIDTH_SCREEN / HEIGHT_SCREEN;
+	float pixelAspect = 8.0f / 16.0f;
+
+	char* screen = new char[WIDTH_SCREEN * HEIGHT_SCREEN + 1];
+	screen[WIDTH_SCREEN * HEIGHT_SCREEN] = '\0';
+	int tmp;
+	for (int k = 0; k < 10000; k++) {
+		for (int i = 0; i < WIDTH_SCREEN; i++){
+			for (int j = 0; j < HEIGHT_SCREEN; j++){
+				tmp = j;
+				float x = (float)i / WIDTH_SCREEN * 2.0f - 1.0f;
+				float y = (float)j / HEIGHT_SCREEN * 2.0f - 1.0f;
+				x *= aspect * pixelAspect;
+				x += sin(k * 0.001);
+				char pixel = ' ';
+				if (x*x + y*y< 1)
+					pixel = '@';
+				if (x * x + y * y < 0.8f)
+					pixel = ' ';
+				screen[i + j * WIDTH_SCREEN] = pixel;	
+				
+			}
+		}
+		printf(screen);
+		
+	}
+	
+
+	
+	getchar();
+}
