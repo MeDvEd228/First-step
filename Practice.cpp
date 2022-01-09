bd133049eb08432bd1d7c222effbcfac9d36b431
MeDvEd228 <88885539+MeDvEd228@users.noreply.github.com>
2022-01-10 00:58:54 +0500

    Update Practice.cpp

--- Practice.cpp
+++ Practice.cpp
@@ -1,38 +1,17 @@
 ﻿#include <iostream>
 
-
-void Delenie() //деление числа на разряды 
-{
-	int NUM = 10819;
-	int arr[5];
-	int i = 0;
-	while (NUM != 0)
-	{
-		arr[i] = NUM % 10;
-		NUM /= 10;
-		i++;
-	}
-	for (int j = 5; j != 0; j--)
-	{
-		std::cout << arr[j - 1] << std::endl;
-	}
-}
-
-
 int main()
 {
-	int const  WIDTH_SCREEN = 120;
-	int const  HEIGHT_SCREEN = 30;
-	float aspect = (float)WIDTH_SCREEN / HEIGHT_SCREEN;
-	float pixelAspect = 8.0f / 16.0f;
+	int const  WIDTH_SCREEN = 120; 	// ширина окна консоли
+	int const  HEIGHT_SCREEN = 30; 	// высота окна консоли 
+	float aspect = (float)WIDTH_SCREEN / HEIGHT_SCREEN; // аспект хранит в себе отношение ширины и высоты 
+	float pixelAspect = 8.0f / 16.0f; 		// пиксельАспект хранит в себе размер одного символа на это влияет размер шрифта в консоли 
 
-	char* screen = new char[WIDTH_SCREEN * HEIGHT_SCREEN + 1];
+	char* screen = new char[WIDTH_SCREEN * HEIGHT_SCREEN + 1];  //выделение памяти для массив чаров формальна вся наша картинка +1 в конце это для нулевого символа
 	screen[WIDTH_SCREEN * HEIGHT_SCREEN] = '\0';
-	int tmp;
-	for (int k = 0; k < 10000; k++) {
+	for (int k = 0; k < 10000; k++) { // анимация длится 10 сек
 		for (int i = 0; i < WIDTH_SCREEN; i++){
 			for (int j = 0; j < HEIGHT_SCREEN; j++){
-				tmp = j;
 				float x = (float)i / WIDTH_SCREEN * 2.0f - 1.0f;
 				float y = (float)j / HEIGHT_SCREEN * 2.0f - 1.0f;
 				x *= aspect * pixelAspect;
@@ -40,17 +19,13 @@ int main()
 				char pixel = ' ';
 				if (x*x + y*y< 1)
 					pixel = '@';
-				if (x * x + y * y < 0.8f)
-					pixel = ' ';
+				if (x * x + y * y < 0.8f) //если убрать эту строчку получится цельный круг 
+					pixel = ' '; // и эту
 				screen[i + j * WIDTH_SCREEN] = pixel;	
 				
 			}
 		}
 		printf(screen);
-		
 	}
-	
-
-	
 	getchar();
 }
